f7f44bfc4cadbc3801d3c8966e4cd27f49184b82
newwmohan-jpg <newwmohan@gmail.com>
2026-07-07 16:54:15 +0530

    added playbar

--- script.js
+++ script.js
@@ -9,15 +9,94 @@ async function Songs() {
         const element = As[index];
         if (element.href.endsWith(".mp3")) {
             songs.push(element.href)
-
         }
     }
     return songs;
 }
-async function main() {
 
-    let songs = await Songs();
-    console.log(songs);
-
+function extractWords(text) {
+    return decodeURIComponent(text)
+        .toLowerCase()
+        .replace(/\.[a-z0-9]+$/i, "")
+        .split(/[\s_\-\/]+/)
+        .filter(Boolean);
 }
-main()
+
+function findBestSong(title, songUrls) {
+    const titleWords = extractWords(title);
+    let bestUrl = null;
+    let bestScore = 0;
+
+    for (const url of songUrls) {
+        const filename = url.substring(url.lastIndexOf("/") + 1);
+        const fileWords = new Set(extractWords(filename));
+        const score = titleWords.filter(w => fileWords.has(w)).length;
+        if (score > bestScore) {
+            bestScore = score;
+            bestUrl = url;
+        }
+    }
+    return bestUrl;
+}
+
+let card = document.querySelectorAll(".song");
+let playbtn = document.querySelector("#plays");
+let currsong = new Audio();
+let isplaying = false;
+let allSongs = [];
+let playBar = document.querySelector(".hiddenPlay")
+
+async function init() {
+    allSongs = await Songs();
+}
+init();
+
+function pauseSong() {
+    currsong.pause();
+    isplaying = false;
+    playbtn.src = "play.png";
+}
+function playSong() {
+    currsong.play();
+    isplaying = true;
+    playbtn.src = "icons8-pause-50.png";
+}
+
+card.forEach(e => {
+    e.addEventListener("click", () => {
+        let title = e.querySelector(".title");
+        let matchedUrl = findBestSong(title.innerHTML, allSongs);
+        
+        if (!matchedUrl) {
+            console.warn("No matching song found for:", title.innerHTML);
+            return;
+        }
+
+        if (currsong.src === matchedUrl && isplaying) {
+            pauseSong();
+
+        } else {
+            playBar.style.display = "contents";
+            currsong.src = matchedUrl;
+            playSong();
+
+        }
+
+    });
+});
+
+plays.addEventListener("click", () => {
+    if (isplaying) {
+       pauseSong();
+
+    }
+    else {
+        playSong();
+    }
+})
+
+// next.addEventListener("click",()=>{
+  
+// currsong = 
+    
+// })
