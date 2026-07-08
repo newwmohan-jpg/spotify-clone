835d7dcf663dc81dd6567a1923deb135fb100da8
newwmohan-jpg <newwmohan@gmail.com>
2026-07-08 23:52:18 +0530

    changed fetch function to fetch from json

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-
 async function Songs() {
-    let a = await fetch("songs/songs.json")
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let As = div.getElementsByTagName("a");
-    let songs = []
-    for (let index = 0; index < As.length; index++) {
-        const element = As[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
-        }
-
-    }
-    return songs
+    let a = await fetch("songs/songs.json");
+    let fileNames = await a.json();
+    let songs = fileNames.map(name => `songs/${name}`);
+    return songs;
 }
 
 
